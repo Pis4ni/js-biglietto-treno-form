02178466f1d4c2cf15a19f77d925b461d1b41516
Pis4ni <pis4wrk@gmail.com>
2023-07-21 10:29:58 +0200

    fix: better order + comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+
+//! --------------------------------------------  ON LOAD  -------------------------------------------- //
+
+
 /* immagazzino il costo/km */
 const costKm = 0.21
 
@@ -13,42 +17,45 @@ const discountMinor = 20
 /* immagazzino il valore dello sconto per maggiore età */
 const discountMajor = 40
 
-
-
-/* ----------------------------------------------------------------------------------------------------------- */
-
+/*  prendo il riferimento dal DOM del bottone */
 let sendBtn = document.getElementById("send_btn")
 
+//! --------------------------------------------  ON CLICK  -------------------------------------------- //
+
+// * FORM //
 
 sendBtn.addEventListener("click", function () {
     let userName = document.getElementById("nameBox").value
     /* chiedo all' utente quanti chilometri ha intenzione di percorrere */
     
 	let userDistance = document.getElementById("user_distance").value // EX PROMPT MODIFIED
-
+    
+    //* CHECK //
 	if (userDistance === null || userDistance.trim() === "") {
-		/* Se l'utente ha cliccato "Annulla" o ha lasciato vuoto il campo, esci dal ciclo*/
-        document.getElementById("error_msg").className ="dispaly"
-		
+        /* Se l'utente ha cliccato "Annulla" o ha lasciato vuoto il campo, esci dal ciclo*/
+        //! DANGER MESSAGE //
+        document.getElementById("error_msg").className ="dispaly" 
 		return
 	}
 
 	distanceNumber = parseFloat(userDistance)
-
+    
 	/* chiedo all' utente la sua età */
-
 	let userAge = document.getElementById("user_age").value
-
+    
+    //* CHECK //
 	if (userAge === null || userAge.trim() === "") {
-		/* Se l'utente ha cliccato "Annulla" o ha lasciato vuoto il campo, esci dal ciclo*/
+        /* Se l'utente ha cliccato "Annulla" o ha lasciato vuoto il campo, esci dal ciclo*/
+        //! DANGER MESSAGE //
         document.getElementById("error_msg").className ="dispaly"
 		return
 	}
-
+    
 	const ageNumber = parseInt(userAge)
-
+    
 	userAge = ageNumber
-
+    
+    //* PRICE CALCS //
 	/* calcolo il prezzo esclusivamente sulla base della distanza/prezzo */
 	let initPrice = costKm * userDistance
 
@@ -60,40 +67,26 @@ sendBtn.addEventListener("click", function () {
 
 	/* immagazzino il valore di quale sconto verrà applicato*/
 	let whichDiscount = 0
-
+    
+    //* PRICE DISCOUNT MINOR //
 	if (userAge < adult) {
-		/* applico uno sconto del 20% sul prezzo totale del bilietto */
+        /* applico uno sconto del 20% sul prezzo totale del bilietto */
 		discountValue = (initPrice * discountMinor) / 100
 		lastPrice = initPrice - discountValue
 		whichDiscount = discountMinor
-		/* ALTRIMENTI SE l' età è compresa tra la maggiore e l' età pensionabile non applico nessuno sconto */
-		/* ALTRIMENTI applico uno sconto pari al 40% */
+        //* PRICE DISCOUNT MINOR //
 	} else if (userAge > retairee) {
+		/* ALTRIMENTI applico uno sconto pari al 40% */
 		discountValue = (initPrice * discountMajor) / 100
 		lastPrice = initPrice - discountValue
 		whichDiscount = discountMajor
 	}
 
-	/* fine. */
-
-	/* mostro a schermo le varie operazioni */
-/* 	document.getElementById("display").innerHTML =
-		"il prezzo iniziale del tuo biglietto è " +
-		initPrice +
-		"€" +
-		" al quale viene applicata la percentuale di sconto del " +
-		whichDiscount +
-		"%" +
-		" il prezzo finale del tuo biglietto è " +
-		lastPrice +
-		"€" +
-		" al quale è stato applicato uno sconto di: " +
-		discountValue +
-		"€" +
-		" Goditi il tuo viaggio!"
- */
     let random = Math.floor(Math.random() * 100000);
     let randomsm = Math.floor(Math.random() * 10);
+    
+	/* fine. */
+    //* MOSTRO A SCHERMO I RISULTATI //
     
     document.getElementById("nameDisplay").innerHTML = userName
     
